57101c567e9ad3d1b6d2b8b6dd6be37791cfdeaf
IbrahimJassat <ibrahim.jassat@dvsa.gov.uk>
2022-05-17 22:24:10 +0100

    Added in tests for the updating an operator availability

---
+++ tests/UpdateATFAvailability.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+import HomePage from '../pages/UpdateATFAvailability';
+
+
+test('Given a Operator wants to update their availability', async ( { page } ) => { 
+  const homePage = new HomePage(page);
+  await homePage.goto();
+
+  await test.step('When the operator update their availability to I have availability', async () => {
+    await homePage.iHaveAvailability();
+  });
+
+  await test.step('Then I should see a message', async () => {
+      await expect(page.locator('text=can take more MOT bookings').first()).toBeVisible();
+    
+  });
+
+});
